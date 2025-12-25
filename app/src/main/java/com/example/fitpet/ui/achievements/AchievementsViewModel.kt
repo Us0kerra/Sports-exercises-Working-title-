ab07b13fc6a70d16c5c360b8553d59b8c1c26dc5
Us0kerra <stesab8@gmail.com>
2025-12-26 03:18:14 +0500

    -

--- app/src/main/java/com/example/fitpet/ui/achievements/AchievementsViewModel.kt
+++ app/src/main/java/com/example/fitpet/ui/achievements/AchievementsViewModel.kt
@@ -2,7 +2,6 @@ package com.example.fitpet.ui.achievements
 
 import androidx.lifecycle.ViewModel
 import com.example.fitpet.data.achievements.Achievement
-import com.example.fitpet.data.achievements.AchievementStatus
 import com.example.fitpet.data.achievements.AchievementsRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -10,20 +9,21 @@ import kotlinx.coroutines.flow.asStateFlow
 
 class AchievementsViewModel : ViewModel() {
 
-    private val repository = AchievementsRepository()
+    private val repository = AchievementsRepository
 
     private val _achievements = MutableStateFlow<List<Achievement>>(emptyList())
     val achievements: StateFlow<List<Achievement>> = _achievements.asStateFlow()
 
     init {
+        loadAchievements()
+    }
+
+    private fun loadAchievements() {
         _achievements.value = repository.getAchievements()
     }
 
     fun claimReward(id: Int) {
-        _achievements.value = _achievements.value.map { ach ->
-            if (ach.id == id && ach.status == AchievementStatus.DONE_NOT_CLAIMED) {
-                ach.copy(status = AchievementStatus.DONE_CLAIMED)
-            } else ach
-        }
+        repository.claimAchievement(id)
+        loadAchievements()
     }
 }
